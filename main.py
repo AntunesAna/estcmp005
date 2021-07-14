d202485341a65ec1bbf19e0c56f6eba0d6c6702d
Ana Jessye Alemeida Antunes <83785342+AntunesAna@users.noreply.github.com>
2021-07-14 13:25:01 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from Pessoa import Pessoa
-from Empregado import Empregado
-
-antonio = Pessoa("Antonio", 55, "Espanha")
-antonio.descricao()
-
-manuel = Empregado(1500.15, "Manuel", "informática")
-manuel.descricao()
